eaa1ca3eb92964e7ebabc3d8d1e58e62862c7bd6
redHurt <redhead112358@gmail.com>
2023-04-15 02:14:11 +0300

    moving to server-base WIP

---
+++ Domain/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Domain;
+
+public class Character
+{
+    private Vector3 _position;
+
+    public Character(Vector3 origin)
+    {
+        _position = origin;
+    }
+
+    public void Move(Vector3 delta)
+    {
+        _position += delta;
+    }
+}
